5b1de1178c9a42b091f40b0c0c612bce19acb95e
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-10-22 15:12:40 +0200

    Add playRound and bestOf5 functions, create comments that explain them

--- rpsGame.js
+++ rpsGame.js
@@ -6,32 +6,93 @@
 // 4. Game is looped to play best of 5
 // 5. Game decides the overall winner based on points
 
+bestOf5();
 
+
+//Function that allows you to play a best of 5 of the game and decides whos the overall winner
+function bestOf5() {
+
+    let round = 5;
+    let userScore = 0;
+    let aiScore = 0;
+
+    for (round = 0; round < 5; round++) {
+
+        let decider = playRound();
+
+        if (decider === 'ai wins') {
+            aiScore++, console.log('ai wins this one'), console.log(aiScore);
+        } else if (decider === 'noone wins') {
+            userScore + 0, console.log('this ones a draw');
+        } else if (decider === 'you win') {
+            userScore++, console.log('you won this round!'), console.log(userScore);
+        }
+    }
+
+    if (userScore > aiScore) {
+        console.log('You won the game!');
+    } else if (aiScore > userScore) {
+        console.log('You lost the game!');
+    } else if (aiScore === userScore) {
+        console.log('Noone wins, its a draw!');
+    }
+}
+
+//Function that allows you to play a single round and decides who won it
+function playRound() {
+
+    let ai = computerChoise();
+    let user = userChoise();
+
+    if (ai === user) {
+        return 'noone wins';
+
+    } else if (ai === 'rock' && user === 'paper') {
+        return 'you win';
+    } else if (ai === 'rock' && user === 'scissors') {
+        return 'ai wins';
+
+
+    } else if (ai === 'paper' && user === 'rock') {
+        return 'ai wins';
+    } else if (ai === 'paper' && user === 'scissors') {
+        return 'you win';
+
+
+    } else if (ai === 'scissors' && user === 'rock') {
+        return 'you win';
+    } else if (ai === 'scissors' && user === 'paper') {
+        return 'ai wins';
+    }
+}
+
+//function that lets user choose what to play
 function userChoise() {
-    
-    choise = prompt ('Choose between Rock, Paper or Scissors!')
 
-    if (choise.toLowerCase() === 'scissors'){
-        return 'Scissors';
-    } else if (choise.toLowerCase() === 'rock'){
-        return 'Rock';
-    } else if (choise.toLowerCase() === 'paper'){
-        return 'Paper';
-    } else{
-        alert ('Try again!'), userChoise();
+    let choise = prompt('Choose between Rock, Paper or Scissors!')
+
+    if (choise.toLowerCase() === 'scissors') {
+        return 'scissors';
+    } else if (choise.toLowerCase() === 'rock') {
+        return 'rock';
+    } else if (choise.toLowerCase() === 'paper') {
+        return 'paper';
+    } else {
+        alert('Try again!'), userChoise();
     }
 
 }
 
+//function that allows the computer to choose what to play
 function computerChoise() {
 
-    choise = Math.floor(Math.random() * 3) + 1;
+    let choise = Math.floor(Math.random() * 3) + 1;
 
     if (choise === 1) {
-        return 'Scissors';
+        return 'scissors';
     } else if (choise === 2) {
-        return 'Rock';
+        return 'rock';
     } else {
-        return 'Paper';
+        return 'paper';
     }
 }
