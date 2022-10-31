a3655e5fd778ab73da94eb08b5e16ffad1ddc6da
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-10-31 17:57:00 +0100

    Create spans to track score, create new icon buttons, fix announcment display

--- rpsGame.js
+++ rpsGame.js
@@ -1,53 +1,64 @@
+const buttons = document.querySelectorAll('.humanChoises');
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound,);
+});
 
-//Function that allows you to play a single round and decides who won it
+
+let verdict = document.querySelector('.win-lose-tie')
+let pcScore = document.querySelector('#aiScore')
+let humanScore = document.querySelector('#userScore')
+let userScore = 0;
+let aiScore = 0;
+
 function playRound(userChoise) {
 
     let ai = computerChoise();
     let user = userChoise.srcElement.id;
-    
-    const announcmentContainer = document.querySelector('.win-lose-tie');
-    
-    const won = document.createElement('h1');
-    won.classList.add('Won');
-    announcmentContainer.appendChild(won);
 
-    const lose = document.createElement('h1');
-    lose.classList.add('Won');
-    announcmentContainer.appendChild(lose);
 
-    const tie = document.createElement('h1');
-    tie.classList.add('Won');;
-    announcmentContainer.appendChild(tie);
 
     if (ai === user) {
-             tie.textContent = 'its a tie!';;
+        verdict.textContent = 'its a tie!';;
 
     } else if (ai === 'rock' && user === 'paper') {
-         won.textContent = 'you won the round', userScore++;
+        verdict.textContent = 'you won the round', userScore++;
+        humanScore.textContent = +humanScore.textContent + 1;
+
     } else if (ai === 'rock' && user === 'scissors') {
-         lose.textContent = 'you lost the round', aiScore++;
+        verdict.textContent = 'you lost the round', aiScore++;
+        pcScore.textContent = +pcScore.textContent + 1;
 
 
     } else if (ai === 'paper' && user === 'rock') {
-         lose.textContent = 'you lost the round', aiScore++;
-    } else if (ai === 'paper' && user === 'scissors') {
-         won.textContent = 'you won the round', userScore++;
+        verdict.textContent = 'you lost the round', aiScore++;
+        pcScore.textContent = +pcScore.textContent + 1;
 
+    } else if (ai === 'paper' && user === 'scissors') {
+        verdict.textContent = 'you won the round', userScore++;
+        humanScore.textContent = +humanScore.textContent + 1;
 
     } else if (ai === 'scissors' && user === 'rock') {
-        return won.textContent = 'you won the round', userScore++;
+        verdict.textContent = 'you won the round', userScore++;
+        humanScore.textContent = +humanScore.textContent + 1;
+
     } else if (ai === 'scissors' && user === 'paper') {
-         lose.textContent = 'you lost the round', aiScore++;
+        verdict.textContent = 'you lost the round', aiScore++;
+        pcScore.textContent = +pcScore.textContent + 1;
+
     }
 
-    if (userScore === 5){
-        return alert ('You won!')
-    } else if (aiScore === 5){
-        return alert ('You lost!')
+    if (userScore === 5) {
+        return alert('You won!'),
+            pcScore.textContent = 0,
+            humanScore.textContent = 0;
+
+    } else if (aiScore === 5) {
+        return alert('You lost!'),
+            pcScore.textContent = 0,
+            humanScore.textContent = 0;
     }
 }
 
-//function that allows the computer to choose what to play
 function computerChoise() {
 
     let choise = Math.floor(Math.random() * 3) + 1;
@@ -61,9 +72,3 @@ function computerChoise() {
     }
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', playRound);
-});
-let userScore = 0;
-let aiScore = 0;
